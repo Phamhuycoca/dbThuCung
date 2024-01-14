af05474bd76ec61f37ea169003526181ce0507b3
PhamHuy <phamkhachuy240702@gmail.com>
2024-01-15 00:51:57 +0700

    ok

--- dbThuCung.Api/Program.cs
+++ dbThuCung.Api/Program.cs
@@ -135,12 +135,12 @@ var cloudinary = new Cloudinary(cloudinaryAccount);
 builder.Services.AddSingleton(cloudinary);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// Configure the HTTP request pipeline./*
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("VueApp");
